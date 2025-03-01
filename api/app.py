5884c8546cefc39a2a3ae12ba91a7229af9704e3
merchantsons <merchantsons@outlook.com>
2025-03-02 01:21:52 +0500

    Final

--- api/app.py
+++ api/app.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__,
             static_folder=os.path.join(BASE_DIR, '../static'),
             template_folder=os.path.join(BASE_DIR, '../templates'))
+
 app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET'])
